942c14f0183c4b3e3765ac0dc5adb93464c35735
tippitytapp <marc.tapp@gmail.com>
2020-05-11 08:04:37 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const server = require("./api/server.js");
+const colors= require('colors');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4666;
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+if(process.env.NODE_ENV === 'development'){
+  server.use(morgan('dev'))
+}
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+  console.log(`\n Server running in ${NODE_ENV} mode on http://localhost:${PORT}...\n`.rainbow.underline.bold);
 });
